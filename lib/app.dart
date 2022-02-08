6de0384b1271138a710462fde6e1135e17a65705
Olexandr Solodchenko <olexandr.solodchenko@i3engineering.com>
2022-02-08 14:04:16 +0200

    test

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'feature_01/feature_01.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.grey,
+        ),
+        home: const HomePage(),
+      );
+}
---
+++ lib/feature_01/services/graph_ql.dart
@@ -0,0 +1,42 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'dart:developer' as dev;
+
+class GraphQLService {
+  GraphQLClient? _client;
+
+  GraphQLService() {
+    HttpLink link = HttpLink(uri: 'https://api.spacex.land/graphql/');
+
+    _client = GraphQLClient(
+      link: link,
+      cache: InMemoryCache(),
+    );
+  }
+
+  Future<QueryResult> performQuery(
+    String query, {
+    required Map<String, dynamic> variables,
+  }) async {
+    QueryOptions options = QueryOptions(document: query, variables: variables);
+
+    final result = await _client!.query(options);
+
+    return result;
+  }
+
+  Future<QueryResult> performMutation(
+    String query, {
+    required Map<String, dynamic> variables,
+  }) async {
+    MutationOptions options = MutationOptions(
+      document: query,
+      variables: variables,
+    );
+
+    final result = await _client!.mutate(options);
+    dev.log(result.toString());
+
+    return result;
+  }
+}
