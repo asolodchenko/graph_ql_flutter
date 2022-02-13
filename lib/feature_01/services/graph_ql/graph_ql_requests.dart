54e2c28a2f515928f647421f44960e508cca8065
Olexandr Solodchenko <olexandr.solodchenko@i3engineering.com>
2022-02-14 01:00:55 +0200

    feat: add limit param

--- lib/feature_01/services/graph_ql/graph_ql_requests.dart
+++ lib/feature_01/services/graph_ql/graph_ql_requests.dart
@@ -1,7 +1,7 @@
 class GqlQueries {
   static const launchesPast = """ 
-query posts(\$search: String){
-  launches(limit: 10, offset: 0, find: {mission_name: \$search}) {
+query posts(\$search: String, \$limit: Int){
+  launches(limit: \$limit, offset: 0, find: {mission_name: \$search}) {
     mission_name
     details
   }
